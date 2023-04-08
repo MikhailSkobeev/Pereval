565c085eadc4f15039b589107bc5238cf583b29c
MikhailSkobeev <111576958+MikhailSkobeev@users.noreply.github.com>
2023-04-08 12:45:08 +0300

    Update apps.py

--- PL/pereval/apps.py
+++ PL/pereval/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PerevalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.DB.models.BigAutoField'
     name = 'pereval'
